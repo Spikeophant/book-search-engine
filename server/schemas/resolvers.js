3f2e1cb52d1dea4b14e7628e17599c2b8b360de6
Dustin Davisson <dustin.davisson@gmail.com>
2022-12-04 16:26:27 -0800

    Queries and starting mutations in resolvers.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,29 @@
-const { Class } = require('../models');
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-
-};
+  Query: {
+    //pass in parent (_) arguments and the current users context.
+    me: async (_, args, context) => {
+      if (context.user) {
+        //return the password and __v
+        const user = await User.findById(context.user._id).select('-__v -password');
+        return user;
+      }
+      //errrrrrror
+      throw new AuthenticationError('Unauthorized');
+    }
+  },
+  Mutation: {
+    createUser: async (_, args) => {
+      const user = await User.create(args);
+      //generate jwt.
+      const token = signToken(user);
+      //return both user and jwt.
+      return { token, user};
+    }
+  }
+}
 
 module.exports = resolvers;
