0e1462f9738becf86896831d151c5dc59692787a
Dustin Davisson <dustin.davisson@gmail.com>
2022-12-04 19:45:18 -0800

    Fix typo on useMutation import.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 //let's kill this and bring in apollo
 //import { loginUser } from '../utils/API';
-import { userMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
