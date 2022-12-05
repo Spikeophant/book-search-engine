9e61d20ab2f98fb338b0c09ff1f84abfe8e26ed8
Dustin Davisson <dustin.davisson@gmail.com>
2022-12-04 17:51:38 -0800

    Apollo on the client App side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+//import aplllo stuff here.
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+
+//initialize apollo client. set the cache.  Setup context.
+//make sure token is set and passed as an authorization header.
+const apolloClient = new ApolloClient({
+  request: operation => {
+    //was going to import the AuthService for this, however it seemed heavy.
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
