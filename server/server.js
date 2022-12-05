d8f14b4fd2f33dbafb149fecee444db8a83e3f82
Dustin Davisson <dustin.davisson@gmail.com>
2022-12-04 19:37:38 -0800

    Cut server all the way to graphql.  Push through client and test.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 //leave these for now, but!!
-// TODO: remove this once front end refactor is complete.
-const routes = require('./routes');
+// Take these out no and test.
+//const routes = require('./routes');
 //import the auth middleware here so we can have context.
 const { authMiddleware } = require('./utils/auth')
 //Add apollo server here.
@@ -27,14 +27,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 //so while I test, if we have routes set
-if (routes) {
+//if (routes) {
   //use the routes if we have them.
-  app.use(routes);
+  //app.use(routes);
   //if not, use apollo.
-} else {
+//} else {
   //if we use apollo, just serve the front end.  This line is technically too long but looks way better like this.
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/buildindex.html')));
-}
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/buildindex.html')));
+//}
 //Create Apollo server instance.  maybe we can just do this without the const, and inline it?  I'll try after it all works.
 // TODO: make this slicker.
 const startApolloServer = async ( typeDefs, resolvers ) => {
